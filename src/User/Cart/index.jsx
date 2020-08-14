491f83753f88f6d44cde12fd5a69cd5a800e9837
Shreya <shreyakvaidya@gmail.com>
2020-08-14 16:34:04 +0530

    Added frontend boiler plate for User side

---
+++ src/User/Cart/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Header";
+
+class Cart extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Cart</h1>
+      </div>
+    );
+  }
+}
+
+export default Cart;
