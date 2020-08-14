a4d4b9f7f54024665663320945f612fbbf6326af
Sameer Kavthekar <kavthekarsam@gmail.com>
2020-08-14 15:45:39 +0530

    Added backend

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// const passport = require("passport");
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
+
+// app.use(passport.initialize());
+
+// require("./config/passport")(passport);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
